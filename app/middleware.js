2fb51cd1823232f777d9ba1e88edd7404fa962b7
medpact <medpact.guntur@gmail.com>
2026-05-05 14:12:26 +0530

    Update middleware.js

--- app/middleware.js
+++ app/middleware.js
@@ -4,24 +4,24 @@ export function middleware(request) {
   const host = request.headers.get('host') || ''
   const pathname = request.nextUrl.pathname
 
-  // 🌍 Medical Tourism
+  console.log("HOST:", host)
+  console.log("PATH:", pathname)
+
   if (host.includes('care.medpact.in')) {
     const path = pathname === "/" ? "" : pathname
+
     return NextResponse.rewrite(
       new URL(`/medicaltourism${path}`, request.url)
     )
   }
 
-  // 📊 Dashboard
   if (host.includes('dashboard.medpact.in')) {
-    return NextResponse.rewrite(
-      new URL(`${pathname}`, request.url)
-    )
+    return NextResponse.next()
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|favicon.ico).*)'],
+  matcher: ['/', '/((?!api|_next|favicon.ico).*)'],
 }
